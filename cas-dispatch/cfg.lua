749d939a49bd784f1f0ceb0e706b924122202941
Fatih <68826839+mb-later@users.noreply.github.com>
2023-07-03 14:17:09 +0300

    Update cfg.lua

--- cas-dispatch/cfg.lua
+++ cas-dispatch/cfg.lua
@@ -6,3 +6,47 @@ Config.PolicePerm = "police"
 Config.UnknownName = "Unknown"
 
 Config.SetWaypoint = "Z"
+
+Config.DriveByHeader = "Drive By Shooting"
+Config.ShotAlert = {
+    header = "Gun Shot",
+    event = "Weapon Model",
+    callsign = "33-50",
+    forwho = "police",
+    blips = {
+        blipText = "Gun Shot",
+        blipSprite = 153,
+        blipColour = 71,
+        blipScale = 0.7,
+        blipTime = 2,
+    }
+}
+
+Config.CarJackingAlert = {
+    header = "Car Jacking",
+    event = "",
+    callsign = "21-49",
+    forwho = "police",
+    blips = {
+        blipText = "Car Jacking",
+        blipSprite = 153,
+        blipColour = 71,
+        blipScale = 0.7,
+        blipTime = 2,
+    }
+}
+
+
+Config.CivDown = {
+    header = "Person Down",
+    event = "",
+    callsign = "55-88",
+    forwho = {"police", "ems"},
+    blips = {
+        blipText = "Shutdown",
+        blipSprite = 153,
+        blipColour = 71,
+        blipScale = 0.7,
+        blipTime = 2,
+    }
+}
